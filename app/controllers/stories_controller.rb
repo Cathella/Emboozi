a57034e0c03a2a01630d454e7047be6418fd001e
Cathella <ncathie214@gmail.com>
2021-04-17 11:14:23 +0300

    redirect to stories after login

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,6 @@
 class StoriesController < ApplicationController
   before_action :set_story, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /stories or /stories.json
   def index
