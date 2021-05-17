d91359e00ee19e4910af57a038888ea966934b70
Cathella <ncathie214@gmail.com>
2021-05-17 22:36:38 +0300

    update controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :stories do
     resources :comments, module: :stories
   end
   
-  devise_for :users
-  get 'home/index'
   root to: 'home#index'
 end
